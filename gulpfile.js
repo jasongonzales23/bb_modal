29b1ac34e20aa1d51ecf1433f584c6ff3c2548d5
Jason Gonzales <jason.gonzales23@gmail.com>
2015-06-16 00:33:26 -0700

    commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var
+  gulp = require('gulp'),
+  browserSync = require('browser-sync').create(),
+  sass = require('gulp-sass'),
+  minifyCSS = require('gulp-minify-css'),
+  autoprefixer = require('gulp-autoprefixer'),
+  rename = require("gulp-rename");
+
+var config = {
+  files: './stylesheets/main.css'
+};
+
+gulp.task('sass', function() {
+  gulp.src('./build/main.scss')
+    .pipe(sass(
+      {
+        style: 'expanded',
+        sourceComments: true
+      }
+    ))
+    .pipe(gulp.dest('./stylesheets/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: "./"
+  });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function() {
+  gulp.watch('./build/**/*.scss', ['sass']);
+});
+
+
+
