0de7bd72e4cf74b94f05e65e100a12acb5798d42
Jason Gonzales <jason.gonzales23@gmail.com>
2015-06-16 07:33:54 -0700

    I am kind of obsessed

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+//TODO calculate this instead of hardcoding
 var marginMultiplier = 272;
 
 var slide = function(e) {
+  e.preventDefault();
   var data = parseInt(e.target.dataset.slide);
   var leftPos = data * marginMultiplier * -1;
   $('.screenshot-images').css({"transform":"translate(" + leftPos +"px ,0)"});
